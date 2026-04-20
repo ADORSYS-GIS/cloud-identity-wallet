eb13903998c5c2b0523b8ad8f58e7165eb06f8d1
Hermann <hermannoenpause@gmail.com>
2026-04-20 10:27:56 +0100

    feat(utils): add json string serialization helper

--- cloud-wallet-openid4vc/src/issuance/utils/mod.rs
+++ cloud-wallet-openid4vc/src/issuance/utils/mod.rs
@@ -1,1 +1,14 @@
 pub mod pkce;
+
+use serde::Serialize;
+use serde::Serializer;
+
+/// Serialize a value as a JSON string
+pub fn serialize_json_string<S, T>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
+where
+    T: Serialize,
+    S: Serializer,
+{
+    let j = serde_json::to_string(value).map_err(serde::ser::Error::custom)?;
+    serializer.serialize_str(&j)
+}
