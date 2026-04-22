9775588854477e0b4db42ebfb5999a8787e9227e
Hermann <hermannoenpause@gmail.com>
2026-04-22 09:27:35 +0100

    refactor(session): modularize session management with storage interface

---
+++ src/session/error.rs
@@ -0,0 +1,17 @@
+use std::borrow::Cow;
+
+/// Session errors.
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Failure in storage backend: {0}")]
+    Store(#[source] Box<dyn std::error::Error + Send + Sync>),
+
+    #[error("Encoding or decoding error: {0}")]
+    Encoding(#[from] postcard::Error),
+
+    #[error("Invalid state transition from {0} to {1}")]
+    InvalidStateTransition(Cow<'static, str>, Cow<'static, str>),
+
+    #[error("{0}")]
+    Other(color_eyre::eyre::Report),
+}
