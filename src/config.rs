d23af8082cad3967f15f7ce3e3fc95c6f7b2f1cc
Hermann <hermannoenpause@gmail.com>
2026-05-04 09:06:16 +0100

    feat: add oid4vci configuration and Redis RESP3 support with push notifications

--- src/config.rs
+++ src/config.rs
@@ -7,12 +7,15 @@ use redis::{
 };
 use secrecy::{ExposeSecret, SecretString};
 use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc::UnboundedReceiver;
+use url::Url;
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Config {
     pub server: ServerConfig,
     pub redis: RedisConfig,
     pub database: DatabaseConfig,
+    pub oid4vci: Oid4vciConfig,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -28,8 +31,13 @@ pub struct RedisConfig {
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct DatabaseConfig {
-    /// Database URL
     pub url: SecretString,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Oid4vciConfig {
+    pub client_id: String,
+    pub redirect_uri: Url,
 }
 
 impl RedisConfig {
@@ -37,16 +45,25 @@ impl RedisConfig {
     ///
     /// - To enable TLS, the URI must use the `rediss://` scheme.
     /// - To enable insecure TLS, the URI must use the `rediss://` scheme and end with `/#insecure`.
+    /// - To enable RESP3 protocol, the URI must contains the `protocol=resp3` query parameter.
     ///
     /// # Errors
     /// Returns an error if the connection cannot be established.
-    pub async fn start(&self) -> RedisResult<ConnectionManager> {
+    pub async fn start(
+        &self,
+    ) -> RedisResult<(ConnectionManager, UnboundedReceiver<redis::PushInfo>)> {
         let client = RedisClient::open(self.uri.expose_secret())?;
+        let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+
         let config = ConnectionManagerConfig::new()
             .set_number_of_retries(3)
             .set_response_timeout(Some(Duration::from_secs(30)))
-            .set_connection_timeout(Some(Duration::from_secs(30)));
-        client.get_connection_manager_with_config(config).await
+            .set_connection_timeout(Some(Duration::from_secs(30)))
+            .set_push_sender(tx)
+            .set_automatic_resubscription();
+
+        let manager = client.get_connection_manager_with_config(config).await?;
+        Ok((manager, rx))
     }
 }
 
@@ -82,8 +99,10 @@ impl Config {
         ConfigLib::builder()
             .set_default("server.host", "127.0.0.1")?
             .set_default("server.port", 3000)?
-            .set_default("redis.uri", "redis://127.0.0.1:6379")?
-            .set_default("database.url", "sqlite::memory:")
+            .set_default("redis.uri", "redis://127.0.0.1:6379?protocol=resp3")?
+            .set_default("database.url", "sqlite::memory:")?
+            .set_default("oid4vci.client_id", "cloud-identity-wallet")?
+            .set_default("oid4vci.redirect_uri", "http://localhost:3000/callback")
     }
 }
 
