ea7ab4cff3a1d07d93d3ed746f21d0c07415835a
martcpp <mordecaiemmanueletukudo@gmail.com>
2026-05-10 11:07:38 +0100

    fix cargo fmt

--- src/server/handlers.rs
+++ src/server/handlers.rs
@@ -5,6 +5,9 @@ mod tenant;
 
 // Export handlers for use in the server
 pub use health::health_check;
-pub use issuance::{authorization_callback, get_credential, get_session_events, list_credentials, start_issuance, submit_consent, submit_transaction_code};
+pub use issuance::{
+    authorization_callback, get_credential, get_session_events, list_credentials, start_issuance,
+    submit_consent, submit_transaction_code,
+};
 pub use root::home;
 pub use tenant::register_tenant;
